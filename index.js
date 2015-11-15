f53cdc5612f72e913795aad35447badeab2692a8
Dani Latorre <dani@danilat.com>
2015-11-15 17:20:40 +0100

    added a list in console to get all the clients in toggl

--- index.js
+++ index.js
@@ -20,3 +20,21 @@ var server = app.listen((process.env.PORT || 3000), function () {
 
   console.log('Example app listening at http://%s:%s', host, port);
 });
+
+
+var auth = {
+  'auth': {
+    'user': token,
+    'pass': 'api_token'
+  }
+}
+request.get("https://www.toggl.com/api/v8/clients", auth, function(err, response, body){
+  if(response.statusCode == 200){
+    var clients = JSON.parse(body);
+    console.log('Your clients:')
+    for(key in clients){
+      console.log('Id for %s: %s', clients[key].name, clients[key].id)
+    }
+  }
+  
+});
