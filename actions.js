a425132d48d8cc128c03c27fca8a1724368c2df8
Dani Latorre <dani@danilat.com>
2015-11-15 17:43:38 +0100

    all the toggle related code moved to their proper file

--- actions.js
+++ actions.js
@@ -2,6 +2,7 @@ var workspace = process.env.WORKSPACE;
 var token = process.env.TOKEN;
 var auth = require('./lib/auth');
 var request = require('request');
+var toggl = require('./lib/toggl');
 
 var firstOfMonth = function(date){
   date.setDate(1);
@@ -13,39 +14,19 @@ var firstOfMonth = function(date){
 var actions = function(app){
   app.get('/:id', auth, function (req, res) {
     var since = firstOfMonth(new Date());
-    var client = req.params.id;
-    var detailtedReportUrl = "https://toggl.com/reports/api/v2/details?workspace_id=" + workspace + "&since=" + since + "&client_ids="+ client +"&user_agent=api_test";
-    var auth = {
-      'auth': {
-        'user': token,
-        'pass': 'api_token'
-      }
-    }
-    var clientUrl = 'https://www.toggl.com/api/v8/clients/' + client;
-    request.get(clientUrl, auth, function(err, response, body){
-      if(response.statusCode == 200){
-        var customer = JSON.parse(body).data;
-        request.get(detailtedReportUrl, auth, function (err, response, body) {
-          body = JSON.parse(body);
-          res.render('index', { task_entries: body.data, since: since, client: customer});
+    toggl.client(req.params.id, function(err, client){
+      if(!err){
+        toggl.report(req.params.id, since, function (err, tasks) {
+          res.render('index', { task_entries: tasks, since: since, client: client});
         });
       }else{
         res.status(404).send('Not found');
       }
     });
   });
-
   app.get('/pdf/:id', function (req, res) {
     var since = firstOfMonth(new Date());
-    var client = req.params.id;
-    var detailtedPdfReportUrl = "https://toggl.com/reports/api/v2/details.pdf?workspace_id=" + workspace + "&since=" + since + "&client_ids="+ client +"&user_agent=api_test";
-    var auth = {
-      'auth': {
-        'user': token,
-        'pass': 'api_token'
-      }
-    }
-    request.get(detailtedPdfReportUrl, auth).pipe(res);
+    toggl.pdfReport(req.params.id, since, res);
   });
 }
 
--- index.js
+++ index.js
@@ -6,12 +6,12 @@ if(process.env.NODE_ENV != 'production'){
 }
 var app = express();
 
-var workspace = process.env.WORKSPACE;
-var token = process.env.TOKEN;
+var toggl = require('./lib/toggl')
 
 app.locals.moment = require('moment');
 app.set('view engine', 'jade');
 app.use(express.static('public'));
+
 require('./actions')(app);
 
 var server = app.listen((process.env.PORT || 3000), function () {
@@ -19,22 +19,12 @@ var server = app.listen((process.env.PORT || 3000), function () {
   var port = server.address().port;
 
   console.log('Example app listening at http://%s:%s', host, port);
+
 });
 
-
-var auth = {
-  'auth': {
-    'user': token,
-    'pass': 'api_token'
+toggl.clients(function(err, clients){
+  console.log('Your clients:')
+  for(key in clients){
+    console.log('Id for %s: %s', clients[key].name, clients[key].id)
   }
-}
-request.get("https://www.toggl.com/api/v8/clients", auth, function(err, response, body){
-  if(response.statusCode == 200){
-    var clients = JSON.parse(body);
-    console.log('Your clients:')
-    for(key in clients){
-      console.log('Id for %s: %s', clients[key].name, clients[key].id)
-    }
-  }
-  
 });
